f942729f8732c9ffe187931e33137afea54aef8c
stuartamlaing-cmd <stuartamlaing@gmail.com>
2025-10-11 14:45:58 +0100

    Update fixtures.data.js

---
+++ docs/fixtures.data.js
@@ -0,0 +1,156 @@
+/* Generated 2025-10-11T13:45:57.852Z */
+window.FIXTURES = [
+  {
+    "competition": "South Africa Series",
+    "date": "2025-10-12",
+    "timeUK": "Sat Dec 30 1899 07:30:00 GMT+0000 (Greenwich Mean Time)",
+    "location": "Adelaide, Australia",
+    "team1": "Australia",
+    "team2": "South Africa",
+    "watch": "Netball pass"
+  },
+  {
+    "competition": "Constellation Cup",
+    "date": "2025-10-17",
+    "timeUK": "Sat Dec 30 1899 10:30:00 GMT+0000 (Greenwich Mean Time)",
+    "location": "John Cain Arena, Melbourne, Australia",
+    "team1": "Australia",
+    "team2": "New Zealand",
+    "watch": "Netball pass"
+  },
+  {
+    "competition": "Constellation Cup",
+    "date": "2025-10-22",
+    "timeUK": "Sat Dec 30 1899 10:30:00 GMT+0000 (Greenwich Mean Time)",
+    "location": "Qudos Bank Arena, Sydney, Australia",
+    "team1": "Australia",
+    "team2": "New Zealand",
+    "watch": "Netball pass"
+  },
+  {
+    "competition": "Constellation Cup",
+    "date": "2025-10-26",
+    "timeUK": "Sat Dec 30 1899 04:10:00 GMT+0000 (Greenwich Mean Time)",
+    "location": "GLOBOX Arena, Hamilton, New Zealand",
+    "team1": "New Zealand",
+    "team2": "Australia",
+    "watch": "Netball pass"
+  },
+  {
+    "competition": "Constellation Cup",
+    "date": "2025-10-29",
+    "timeUK": "Sat Dec 30 1899 07:40:00 GMT+0000 (Greenwich Mean Time)",
+    "location": "Wolfbrook Arena, Christchurch, New Zealand",
+    "team1": "New Zealand",
+    "team2": "Australia",
+    "watch": "Netball pass"
+  },
+  {
+    "competition": "Vitality Netball International",
+    "date": "2025-11-15",
+    "timeUK": "Sat Dec 30 1899 17:00:00 GMT+0000 (Greenwich Mean Time)",
+    "location": "Copper Box Arena, London",
+    "team1": "England",
+    "team2": "New Zealand",
+    "watch": "Sky Sports"
+  },
+  {
+    "competition": "Vitality Netball International",
+    "date": "2025-11-16",
+    "timeUK": "Sat Dec 30 1899 15:00:00 GMT+0000 (Greenwich Mean Time)",
+    "location": "Copper Box Arena, London",
+    "team1": "England",
+    "team2": "New Zealand",
+    "watch": "Sky Sports"
+  },
+  {
+    "competition": "Vitality Netball International",
+    "date": "2025-11-19",
+    "timeUK": "Sat Dec 30 1899 19:00:00 GMT+0000 (Greenwich Mean Time)",
+    "location": "AO Arena, Manchester",
+    "team1": "England",
+    "team2": "New Zealand",
+    "watch": "Sky Sports"
+  },
+  {
+    "competition": "Celtic Cup",
+    "date": "2025-11-26",
+    "timeUK": "TBC",
+    "location": "Emirates Arena, Glasgow",
+    "team1": "Northern Ireland, Wales, Scotland, Uganda, Zimbabwe, Namibia",
+    "team2": "",
+    "watch": "BBC Sport Scotland Website;BBC iPlayer"
+  },
+  {
+    "competition": "Celtic Cup",
+    "date": "2025-11-27",
+    "timeUK": "TBC",
+    "location": "Emirates Arena, Glasgow",
+    "team1": "Northern Ireland, Wales, Scotland, Uganda, Zimbabwe, Namibia",
+    "team2": "",
+    "watch": "BBC Sport Scotland Website;BBC iPlayer"
+  },
+  {
+    "competition": "Celtic Cup",
+    "date": "2025-11-28",
+    "timeUK": "TBC",
+    "location": "Emirates Arena, Glasgow",
+    "team1": "Northern Ireland, Wales, Scotland, Uganda, Zimbabwe, Namibia",
+    "team2": "",
+    "watch": "BBC Sport Scotland Website;BBC iPlayer"
+  },
+  {
+    "competition": "Celtic Cup",
+    "date": "2025-11-29",
+    "timeUK": "TBC",
+    "location": "Emirates Arena, Glasgow",
+    "team1": "Northern Ireland, Wales, Scotland, Uganda, Zimbabwe, Namibia",
+    "team2": "",
+    "watch": "BBC Sport Scotland Website;BBC iPlayer"
+  },
+  {
+    "competition": "Celtic Cup",
+    "date": "2025-11-30",
+    "timeUK": "TBC",
+    "location": "Emirates Arena, Glasgow",
+    "team1": "Northern Ireland, Wales, Scotland, Uganda, Zimbabwe, Namibia",
+    "team2": "",
+    "watch": "BBC Sport Scotland Website;BBC iPlayer"
+  },
+  {
+    "competition": "The Horizon Series",
+    "date": "2025-12-06",
+    "timeUK": "Sat Dec 30 1899 00:00:00 GMT+0000 (Greenwich Mean Time)",
+    "location": "National Indoor Sports Centre, Kingston, Jamaica",
+    "team1": "Jamaica",
+    "team2": "England",
+    "watch": "BBC Sport"
+  },
+  {
+    "competition": "The Horizon Series",
+    "date": "2025-12-07",
+    "timeUK": "Sat Dec 30 1899 00:00:00 GMT+0000 (Greenwich Mean Time)",
+    "location": "National Indoor Sports Centre, Kingston, Jamaica",
+    "team1": "Jamaica",
+    "team2": "England",
+    "watch": "BBC Sport"
+  },
+  {
+    "competition": "The Horizon Series",
+    "date": "2025-12-13",
+    "timeUK": "Sat Dec 30 1899 14:15:00 GMT+0000 (Greenwich Mean Time)",
+    "location": "Copper Box Arena, London",
+    "team1": "England",
+    "team2": "Jamaica",
+    "watch": "BBC Sport"
+  },
+  {
+    "competition": "The Horizon Series",
+    "date": "2025-12-14",
+    "timeUK": "Sat Dec 30 1899 14:00:00 GMT+0000 (Greenwich Mean Time)",
+    "location": "Copper Box Arena, London",
+    "team1": "England",
+    "team2": "Jamaica",
+    "watch": "BBC Sport"
+  }
+];
