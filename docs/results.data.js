90beb90af04a0718e0b3f060460725c498853397
stuartamlaing-cmd <stuartamlaing@gmail.com>
2025-11-26 21:59:10 +0000

    Update results.data.js

--- docs/results.data.js
+++ docs/results.data.js
@@ -1,4 +1,4 @@
-/* Generated 2025-11-26T21:53:38.245Z */
+/* Generated 2025-11-26T21:59:08.569Z */
 window.RESULTS  = [
   {
     "competition": "Celtic Cup 2025",
@@ -9,7 +9,18 @@ window.RESULTS  = [
     "team2": "Namibia",
     "watch": "Netball Pass",
     "score1": "65",
-    "score2": ""
+    "score2": "38"
+  },
+  {
+    "competition": "Celtic Cup 2025",
+    "date": "2025-11-26",
+    "timeUK": "14:30",
+    "location": "Emirates Arena, Glasgow",
+    "team1": "Uganda",
+    "team2": "Namibia",
+    "watch": "Netball Pass",
+    "score1": "65",
+    "score2": "38"
   },
   {
     "competition": "Celtic Cup 2025",
