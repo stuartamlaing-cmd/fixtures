0a5c31463cdc004b9d0e19fed22fab1a08a4e852
stuartamlaing-cmd <stuartamlaing@gmail.com>
2025-12-02 11:40:05 +0000

    Update fixtures.data.js

--- docs/fixtures.data.js
+++ docs/fixtures.data.js
@@ -1,14 +1,5 @@
-/* Generated 2025-12-02T11:33:20.967Z */
+/* Generated 2025-12-02T11:40:05.244Z */
 window.FIXTURES = [
-  {
-    "competition": "The Horizon Series",
-    "date": "2025-12-13",
-    "timeUK": "14:15",
-    "location": "Copper Box Arena, London",
-    "team1": "England",
-    "team2": "Jamaica",
-    "watch": "BBC TWO; Netball Pass"
-  },
   {
     "competition": "The Horizon Series",
     "date": "2025-12-13",
@@ -26,5 +17,14 @@ window.FIXTURES = [
     "team1": "England",
     "team2": "Jamaica",
     "watch": "Netball Pass"
+  },
+  {
+    "competition": "",
+    "date": "2026-01-29",
+    "timeUK": "",
+    "location": "",
+    "team1": "",
+    "team2": "",
+    "watch": ""
   }
 ];
