3befe31b5c5913f9f5724845d181641701442a22
stuartamlaing-cmd <stuartamlaing@gmail.com>
2026-01-28 20:34:11 +0000

    Update fixtures.data.js

--- docs/fixtures.data.js
+++ docs/fixtures.data.js
@@ -1,4 +1,4 @@
-/* Generated 2026-01-28T20:31:41.553Z */
+/* Generated 2026-01-28T20:34:11.291Z */
 window.FIXTURES = [
   {
     "competition": "SPAR Challenge Series 2026",
@@ -7,7 +7,7 @@ window.FIXTURES = [
     "location": "Ellis Park Indoor Arena",
     "team1": "South Africa",
     "team2": "Vitality Roses",
-    "watch": "Sky Sports; Sky sports Youtube channel"
+    "watch": "Sky Sports; Sky Sports Youtube"
   },
   {
     "competition": "SPAR Challenge Series 2026",
@@ -16,7 +16,7 @@ window.FIXTURES = [
     "location": "Ellis Park Indoor Arena",
     "team1": "South Africa",
     "team2": "Vitality Roses",
-    "watch": "Sky Sports"
+    "watch": "Sky Sports; Sky sports Youtube channel"
   },
   {
     "competition": "SPAR Challenge Series 2026",
@@ -25,7 +25,7 @@ window.FIXTURES = [
     "location": "Ellis Park Indoor Arena",
     "team1": "South Africa",
     "team2": "Vitality Roses",
-    "watch": "Sky Sports"
+    "watch": "Sky Sports; Sky sports Youtube channel"
   },
   {
     "competition": "Australia vs Jamaica Series",
