20fe912b96a7e76bfcaf95655d4ced3e05ca2555
stuartamlaing-cmd <stuartamlaing@gmail.com>
2026-02-05 13:50:29 +0000

    Update fixtures.data.js

--- docs/fixtures.data.js
+++ docs/fixtures.data.js
@@ -1,14 +1,5 @@
-/* Generated 2026-02-01T15:38:51.129Z */
+/* Generated 2026-02-05T13:50:29.124Z */
 window.FIXTURES = [
-  {
-    "competition": "Australia vs Jamaica Series",
-    "date": "2026-02-05",
-    "timeUK": "08:00",
-    "location": "Adelaide Entertainment Centre",
-    "team1": "Australia",
-    "team2": "Jamaica",
-    "watch": "Netball Pass"
-  },
   {
     "competition": "Australia vs Jamaica Series",
     "date": "2026-02-08",
