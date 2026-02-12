791af333e0ad4f8c2b7ab26a685653e127447566
stuartamlaing-cmd <stuartamlaing@gmail.com>
2026-02-12 12:57:51 +0000

    Update fixtures.data.js

--- docs/fixtures.data.js
+++ docs/fixtures.data.js
@@ -1,14 +1,5 @@
-/* Generated 2026-02-09T09:06:40.017Z */
+/* Generated 2026-02-12T12:57:51.267Z */
 window.FIXTURES = [
-  {
-    "competition": "Australia vs Jamaica Series",
-    "date": "2026-02-12",
-    "timeUK": "08:00",
-    "location": "John Cain Arena, Melbourne, Australia",
-    "team1": "Australia",
-    "team2": "Jamaica",
-    "watch": "Netball Pass"
-  },
   {
     "competition": "NSL - Super Cup",
     "date": "2026-02-21",
