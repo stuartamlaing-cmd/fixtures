731e8f959fe85dcbe774f4712980a57576e9c987
stuartamlaing-cmd <stuartamlaing@gmail.com>
2026-02-15 17:21:31 +0000

    Update fixtures.data.js

--- docs/fixtures.data.js
+++ docs/fixtures.data.js
@@ -1,4 +1,4 @@
-/* Generated 2026-02-15T17:10:43.300Z */
+/* Generated 2026-02-15T17:21:30.843Z */
 window.FIXTURES = [
   {
     "competition": "NSL - Super Cup",
@@ -16,7 +16,7 @@ window.FIXTURES = [
     "location": "Utilita Arena, Sheffield",
     "team1": "Manchester Thunder",
     "team2": "NIC Leeds Rhinos",
-    "watch": ""
+    "watch": "SKY sports mix"
   },
   {
     "competition": "NSL - Super Cup",
@@ -25,7 +25,7 @@ window.FIXTURES = [
     "location": "Utilita Arena, Sheffield",
     "team1": "London Pulse",
     "team2": "London Mavericks",
-    "watch": ""
+    "watch": "SKY sports mix"
   },
   {
     "competition": "NSL - Super Cup",
@@ -34,7 +34,7 @@ window.FIXTURES = [
     "location": "Utilita Arena, Sheffield",
     "team1": "Birmingham Panthers",
     "team2": "Lexisnexis Dragons",
-    "watch": ""
+    "watch": "SKY sports mix"
   },
   {
     "competition": "NSL - Super Cup",
@@ -43,7 +43,7 @@ window.FIXTURES = [
     "location": "Utilita Arena, Sheffield",
     "team1": "London Pulse",
     "team2": "Manchester Thunder",
-    "watch": ""
+    "watch": "SKY sports mix"
   },
   {
     "competition": "NSL - Super Cup",
@@ -52,7 +52,7 @@ window.FIXTURES = [
     "location": "Utilita Arena, Sheffield",
     "team1": "Nottingham Forest",
     "team2": "Birmingham Panthers",
-    "watch": ""
+    "watch": "SKY sports mix"
   },
   {
     "competition": "NSL - Super Cup",
@@ -61,7 +61,7 @@ window.FIXTURES = [
     "location": "Utilita Arena, Sheffield",
     "team1": "Lexisnexis Dragons",
     "team2": "Loughborough Lightning",
-    "watch": ""
+    "watch": "SKY sports mix"
   },
   {
     "competition": "NSL - Super Cup",
@@ -70,7 +70,7 @@ window.FIXTURES = [
     "location": "Utilita Arena, Sheffield",
     "team1": "London Mavericks",
     "team2": "NIC Leeds Rhinos",
-    "watch": ""
+    "watch": "SKY sports mix"
   },
   {
     "competition": "NSL - Super Cup",
@@ -79,7 +79,7 @@ window.FIXTURES = [
     "location": "Utilita Arena, Sheffield",
     "team1": "Nottingham Forest",
     "team2": "Lexisnexis Dragons",
-    "watch": ""
+    "watch": "SKY sports mix"
   },
   {
     "competition": "NSL - Super Cup",
@@ -88,7 +88,7 @@ window.FIXTURES = [
     "location": "Utilita Arena, Sheffield",
     "team1": "Manchester Thunder",
     "team2": "London Mavericks",
-    "watch": ""
+    "watch": "SKY sports mix"
   },
   {
     "competition": "NSL - Super Cup",
@@ -97,7 +97,7 @@ window.FIXTURES = [
     "location": "Utilita Arena, Sheffield",
     "team1": "NIC Leeds Rhinos",
     "team2": "London Pulse",
-    "watch": ""
+    "watch": "SKY sports mix"
   },
   {
     "competition": "NSL - Super Cup",
@@ -106,7 +106,7 @@ window.FIXTURES = [
     "location": "Utilita Arena, Sheffield",
     "team1": "Birmingham Panthers",
     "team2": "Loughborough Lightning",
-    "watch": ""
+    "watch": "SKY sports mix"
   },
   {
     "competition": "NSL - Super Cup",
@@ -115,7 +115,7 @@ window.FIXTURES = [
     "location": "Utilita Arena, Sheffield",
     "team1": "1st Group 1",
     "team2": "2nd Group 2",
-    "watch": ""
+    "watch": "SKY sports mix"
   },
   {
     "competition": "NSL - Super Cup",
@@ -124,7 +124,7 @@ window.FIXTURES = [
     "location": "Utilita Arena, Sheffield",
     "team1": "2nd Group 1",
     "team2": "1st Group2",
-    "watch": ""
+    "watch": "SKY sports mix"
   },
   {
     "competition": "NSL - Super Cup",
@@ -133,7 +133,7 @@ window.FIXTURES = [
     "location": "Utilita Arena, Sheffield",
     "team1": "Winner of Semi-Final 1",
     "team2": "Winner of Semi-Final 2",
-    "watch": ""
+    "watch": "SKY sports mix"
   },
   {
     "competition": "NSL- Round 1",
@@ -142,6 +142,501 @@ window.FIXTURES = [
     "location": "Copper Box Arena",
     "team1": "London Pulse",
     "team2": "Manchester Thunder",
-    "watch": "Sky Sports"
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL- Round 1",
+    "date": "2026-02-28",
+    "timeUK": "14:00",
+    "location": "Hertfordshire Sports Village",
+    "team1": "London Mavericks",
+    "team2": "Nottingham Forest",
+    "watch": "BBC Sport; Netball Pass"
+  },
+  {
+    "competition": "NSL- Round 1",
+    "date": "2026-02-28",
+    "timeUK": "18:00",
+    "location": "Sir David Wallace Arena",
+    "team1": "Loughborough Lightning",
+    "team2": "Lexisnexis Dragons",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL- Round 1",
+    "date": "2026-02-28",
+    "timeUK": "19:00",
+    "location": "Worcester Arena",
+    "team1": "Birmingham Panthers",
+    "team2": "NIC Leeds Rhinos",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL- Round 2",
+    "date": "2026-03-06",
+    "timeUK": "19:00",
+    "location": "House Of Sport",
+    "team1": "Lexisnexis Dragons",
+    "team2": "London Pulse",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL- Round 2",
+    "date": "2026-03-06",
+    "timeUK": "19:30",
+    "location": "Canon Medical Arena",
+    "team1": "NIC Leeds Rhinos",
+    "team2": "Loughborough Lightning",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL- Round 2",
+    "date": "2026-03-07",
+    "timeUK": "17:00",
+    "location": "Belle Vue Arena",
+    "team1": "Manchester Thunder",
+    "team2": "London Mavericks",
+    "watch": "BBC Sport; Netball Pass"
+  },
+  {
+    "competition": "NSL- Round 2",
+    "date": "2026-03-08",
+    "timeUK": "16:00",
+    "location": "Motorpoint Arena",
+    "team1": "Nottingham Forest",
+    "team2": "Birmingham Panthers",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL- Round 3",
+    "date": "2026-03-13",
+    "timeUK": "19:30",
+    "location": "House Of Sport",
+    "team1": "Lexisnexis Dragons",
+    "team2": "Manchester Thunder",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL- Round 3",
+    "date": "2026-03-14",
+    "timeUK": "16:00",
+    "location": "Motorpoint Arena",
+    "team1": "Birmingham Panthers",
+    "team2": "London Mavericks",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL- Round 3",
+    "date": "2026-03-14",
+    "timeUK": "18:00",
+    "location": "Motorpoint Arena",
+    "team1": "Loughborough Lightning",
+    "team2": "London Pulse",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL- Round 3",
+    "date": "2026-03-15",
+    "timeUK": "16:00",
+    "location": "Motorpoint Arena",
+    "team1": "Nottingham Forest",
+    "team2": "NIC Leeds Rhinos",
+    "watch": "BBC Sport; Netball Pass"
+  },
+  {
+    "competition": "NSL - Round 4",
+    "date": "2026-03-20",
+    "timeUK": "19:00",
+    "location": "Canon Medical Arena",
+    "team1": "NIC Leeds Rhinos",
+    "team2": "Lexisnexis Dragons",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL - Round 4",
+    "date": "2026-03-22",
+    "timeUK": "15:00",
+    "location": "Utilita Arena",
+    "team1": "Birmingham Panthers",
+    "team2": "Manchester Thunder",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL - Round 4",
+    "date": "2026-03-22",
+    "timeUK": "16:00",
+    "location": "Essex Sports Arena",
+    "team1": "London Mavericks",
+    "team2": "Loughborough Lightning",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL - Round 4",
+    "date": "2026-03-22",
+    "timeUK": "18:00",
+    "location": "Copper Box Arena",
+    "team1": "London Pulse",
+    "team2": "Nottingham Forest",
+    "watch": "BBC Sport; Netball Pass"
+  },
+  {
+    "competition": "NSL - Round 5",
+    "date": "2026-03-27",
+    "timeUK": "19:00",
+    "location": "Canon Medical Arena",
+    "team1": "NIC Leeds Rhinos",
+    "team2": "London Pulse",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL - Round 5",
+    "date": "2026-03-28",
+    "timeUK": "16:00",
+    "location": "House Of Sport",
+    "team1": "Lexisnexis Dragons",
+    "team2": "London Mavericks",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL - Round 5",
+    "date": "2026-03-28",
+    "timeUK": "17:00",
+    "location": "Belle Vue Arena",
+    "team1": "Manchester Thunder",
+    "team2": "Nottingham Forest",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL - Round 5",
+    "date": "2026-03-28",
+    "timeUK": "18:00",
+    "location": "Sir David Wallace Arena",
+    "team1": "Loughborough Lightning",
+    "team2": "Birmingham Panthers",
+    "watch": "BBC Sport; Netball Pass"
+  },
+  {
+    "competition": "NSL- Round 6",
+    "date": "2026-04-04",
+    "timeUK": "15:00",
+    "location": "Motorpoint Arena",
+    "team1": "Nottingham Forest",
+    "team2": "Lexisnexis Dragons",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL- Round 6",
+    "date": "2026-04-04",
+    "timeUK": "17:00",
+    "location": "Copper Box Arena",
+    "team1": "London Pulse",
+    "team2": "Birmingham Panthers",
+    "watch": "BBC Sport; Netball Pass"
+  },
+  {
+    "competition": "NSL- Round 6",
+    "date": "2026-04-04",
+    "timeUK": "17:00",
+    "location": "Motorpoint Arena",
+    "team1": "Loughborough Lightning",
+    "team2": "Manchester Thunder",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL- Round 6",
+    "date": "2026-04-04",
+    "timeUK": "19:00",
+    "location": "Hertfordshire Sports Village",
+    "team1": "London Mavericks",
+    "team2": "NIC Leeds Rhinos",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL - Round 7",
+    "date": "2026-04-10",
+    "timeUK": "19:00",
+    "location": "Copper Box Arena",
+    "team1": "London Pulse",
+    "team2": "London Mavericks",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL - Round 7",
+    "date": "2026-04-11",
+    "timeUK": "16:00",
+    "location": "House Of Sport",
+    "team1": "Lexisnexis Dragons",
+    "team2": "Birmingham Panthers",
+    "watch": "BBC Sport; Netball Pass"
+  },
+  {
+    "competition": "NSL - Round 7",
+    "date": "2026-04-11",
+    "timeUK": "17:00",
+    "location": "Belle Vue Arena",
+    "team1": "Manchester Thunder",
+    "team2": "NIC Leeds Rhinos",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL - Round 7",
+    "date": "2026-04-11",
+    "timeUK": "19:00",
+    "location": "Motorpoint Arena",
+    "team1": "Nottingham Forest",
+    "team2": "Loughborough Lightning",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL - Round 8",
+    "date": "2026-04-17",
+    "timeUK": "19:00",
+    "location": "Worcester Arena",
+    "team1": "Birmingham Panthers",
+    "team2": "Nottingham Forest",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL - Round 8",
+    "date": "2026-04-18",
+    "timeUK": "16:00",
+    "location": "Belle Vue Arena",
+    "team1": "Manchester Thunder",
+    "team2": "Lexisnexis Dragons",
+    "watch": "Sky Sports; Sky Sport YouTube; Netball Pass"
+  },
+  {
+    "competition": "NSL - Round 8",
+    "date": "2026-04-18",
+    "timeUK": "18:00",
+    "location": "Sir David Wallace Arena",
+    "team1": "Loughborough Lightning",
+    "team2": "London Mavericks",
+    "watch": "BBC Sport; Netball Pass"
+  },
+  {
+    "competition": "NSL - Round 8",
+    "date": "2026-04-19",
+    "timeUK": "18:00",
+    "location": "Copper Box Arena",
+    "team1": "London Pulse",
+    "team2": "NIC Leeds Rhinos",
+    "watch": ""
+  },
+  {
+    "competition": "NSL - Round 9",
+    "date": "2026-04-24",
+    "timeUK": "19:00",
+    "location": "Canon Medical Arena",
+    "team1": "NIC Leeds Rhinos",
+    "team2": "Nottingham Forest",
+    "watch": ""
+  },
+  {
+    "competition": "NSL - Round 9",
+    "date": "2026-04-25",
+    "timeUK": "16:00",
+    "location": "Brighton Centre",
+    "team1": "London Mavericks",
+    "team2": "Manchester Thunder",
+    "watch": ""
+  },
+  {
+    "competition": "NSL - Round 9",
+    "date": "2026-04-25",
+    "timeUK": "18:00",
+    "location": "Worcester Arena",
+    "team1": "Birmingham Panthers",
+    "team2": "London Pulse",
+    "watch": ""
+  },
+  {
+    "competition": "NSL - Round 9",
+    "date": "2026-04-26",
+    "timeUK": "16:00",
+    "location": "Utilita Arena",
+    "team1": "Lexisnexis Dragons",
+    "team2": "Loughborough Lightning",
+    "watch": ""
+  },
+  {
+    "competition": "NSL - Round 10",
+    "date": "2026-05-02",
+    "timeUK": "18:00",
+    "location": "House Of Sport",
+    "team1": "Lexisnexis Dragons",
+    "team2": "Nottingham Forest",
+    "watch": ""
+  },
+  {
+    "competition": "NSL - Round 10",
+    "date": "2026-05-02",
+    "timeUK": "19:30",
+    "location": "Canon Medical Arena",
+    "team1": "NIC Leeds Rhinos",
+    "team2": "London Mavericks",
+    "watch": ""
+  },
+  {
+    "competition": "NSL - Round 10",
+    "date": "2026-05-03",
+    "timeUK": "18:00",
+    "location": "Copper Box Arena",
+    "team1": "London Pulse",
+    "team2": "Loughborough Lightning",
+    "watch": ""
+  },
+  {
+    "competition": "NSL - Round 10",
+    "date": "2026-05-04",
+    "timeUK": "18:00",
+    "location": "Belle Vue Arena",
+    "team1": "Manchester Thunder",
+    "team2": "Birmingham Panthers",
+    "watch": ""
+  },
+  {
+    "competition": "NSL Round 11",
+    "date": "2026-05-08",
+    "timeUK": "19:00",
+    "location": "Skydome",
+    "team1": "Birmingham Panthers",
+    "team2": "Loughborough Lightning",
+    "watch": ""
+  },
+  {
+    "competition": "NSL Round 11",
+    "date": "2026-05-09",
+    "timeUK": "15:00",
+    "location": "Motorpoint Arena",
+    "team1": "Nottingham Forest",
+    "team2": "London Mavericks",
+    "watch": ""
+  },
+  {
+    "competition": "NSL Round 11",
+    "date": "2026-05-09",
+    "timeUK": "15:30",
+    "location": "M&S Bank Arena",
+    "team1": "Lexisnexis Dragons",
+    "team2": "NIC Leeds Rhinos",
+    "watch": ""
+  },
+  {
+    "competition": "NSL Round 11",
+    "date": "2026-05-09",
+    "timeUK": "18:00",
+    "location": "M&S Bank Arena",
+    "team1": "Manchester Thunder",
+    "team2": "London Pulse",
+    "watch": ""
+  },
+  {
+    "competition": "NSL Round 12",
+    "date": "2026-05-15",
+    "timeUK": "19:00",
+    "location": "Canon Medical Arena",
+    "team1": "NIC Leeds Rhinos",
+    "team2": "Manchester Thunder",
+    "watch": ""
+  },
+  {
+    "competition": "NSL Round 12",
+    "date": "2026-05-15",
+    "timeUK": "19:00",
+    "location": "Sir David Wallace Arena",
+    "team1": "Loughborough Lightning",
+    "team2": "Nottingham Forest",
+    "watch": ""
+  },
+  {
+    "competition": "NSL Round 12",
+    "date": "2026-05-16",
+    "timeUK": "18:00",
+    "location": "Skydome",
+    "team1": "Birmingham Panthers",
+    "team2": "Lexisnexis Dragons",
+    "watch": ""
+  },
+  {
+    "competition": "NSL Round 12",
+    "date": "2026-05-17",
+    "timeUK": "16:00",
+    "location": "Ovo Arena",
+    "team1": "London Mavericks",
+    "team2": "London Pulse",
+    "watch": ""
+  },
+  {
+    "competition": "NSL Round 13",
+    "date": "2026-05-22",
+    "timeUK": "19:00",
+    "location": "Motorpoint Arena",
+    "team1": "Nottingham Forest",
+    "team2": "Manchester Thunder",
+    "watch": ""
+  },
+  {
+    "competition": "NSL Round 13",
+    "date": "2026-05-23",
+    "timeUK": "18:00",
+    "location": "Sir David Wallace Arena",
+    "team1": "Loughborough Lightning",
+    "team2": "NIC Leeds Rhinos",
+    "watch": ""
+  },
+  {
+    "competition": "NSL Round 13",
+    "date": "2026-05-24",
+    "timeUK": "17:00",
+    "location": "Copper Box Arena",
+    "team1": "London Pulse",
+    "team2": "Lexisnexis Dragons",
+    "watch": ""
+  },
+  {
+    "competition": "NSL Round 13",
+    "date": "2026-05-24",
+    "timeUK": "18:00",
+    "location": "Essex Sports Arena",
+    "team1": "London Mavericks",
+    "team2": "Birmingham Panthers",
+    "watch": ""
+  },
+  {
+    "competition": "NSL Round 14",
+    "date": "2026-05-29",
+    "timeUK": "19:00",
+    "location": "Canon Medical Arena",
+    "team1": "NIC Leeds Rhinos",
+    "team2": "Birmingham Panthers",
+    "watch": ""
+  },
+  {
+    "competition": "NSL Round 14",
+    "date": "2026-05-30",
+    "timeUK": "16:00",
+    "location": "Hertfordshire Sports Village",
+    "team1": "London Mavericks",
+    "team2": "Lexisnexis Dragons",
+    "watch": ""
+  },
+  {
+    "competition": "NSL Round 14",
+    "date": "2026-05-30",
+    "timeUK": "17:00",
+    "location": "National Cycling Centre",
+    "team1": "Manchester Thunder",
+    "team2": "Loughborough Lightning",
+    "watch": ""
+  },
+  {
+    "competition": "NSL Round 14",
+    "date": "2026-05-31",
+    "timeUK": "18:00",
+    "location": "Motorpoint Arena",
+    "team1": "Nottingham Forest",
+    "team2": "London Pulse",
+    "watch": ""
   }
 ];
