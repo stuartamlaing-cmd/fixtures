83541753f44ca59da43fbc6154fbc920cdcad880
stuartamlaing-cmd <stuartamlaing@gmail.com>
2026-02-21 17:28:06 +0000

    Update fixtures.data.js

--- docs/fixtures.data.js
+++ docs/fixtures.data.js
@@ -1,14 +1,5 @@
-/* Generated 2026-02-21T17:24:27.117Z */
+/* Generated 2026-02-21T17:28:05.782Z */
 window.FIXTURES = [
-  {
-    "competition": "NSL - Super Cup",
-    "date": "2026-02-21",
-    "timeUK": "16:50",
-    "location": "Utilita Arena, Sheffield",
-    "team1": "Birmingham Panthers",
-    "team2": "Loughborough Lightning",
-    "watch": "Sky Sports mix; Sky Sports YouTube; Netball Pass"
-  },
   {
     "competition": "NSL - Super Cup",
     "date": "2026-02-21",
