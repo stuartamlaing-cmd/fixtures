58d7f2915515f5c72cb04cf0c3dd85d14a6886a2
stuartamlaing-cmd <stuartamlaing@gmail.com>
2026-05-30 19:52:13 +0100

    Update fixtures.data.js

--- docs/fixtures.data.js
+++ docs/fixtures.data.js
@@ -1,14 +1,5 @@
-/* Generated 2026-05-29T19:38:45.200Z */
+/* Generated 2026-05-30T18:52:13.416Z */
 window.FIXTURES = [
-  {
-    "competition": "NSL Round 14",
-    "date": "2026-05-30",
-    "timeUK": "16:00",
-    "location": "Hertfordshire Sports Village",
-    "team1": "London Mavericks",
-    "team2": "Lexisnexis Dragons",
-    "watch": ""
-  },
   {
     "competition": "NSL Round 14",
     "date": "2026-05-30",
