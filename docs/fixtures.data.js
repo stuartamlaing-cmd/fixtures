1cad04f4c5911c0549fdc8c35de7ab31bb41540c
stuartamlaing-cmd <stuartamlaing@gmail.com>
2026-05-30 19:52:44 +0100

    Update fixtures.data.js

--- docs/fixtures.data.js
+++ docs/fixtures.data.js
@@ -1,14 +1,5 @@
-/* Generated 2026-05-30T18:52:13.416Z */
+/* Generated 2026-05-30T18:52:44.046Z */
 window.FIXTURES = [
-  {
-    "competition": "NSL Round 14",
-    "date": "2026-05-30",
-    "timeUK": "17:00",
-    "location": "National Cycling Centre",
-    "team1": "Manchester Thunder",
-    "team2": "Loughborough Lightning",
-    "watch": ""
-  },
   {
     "competition": "NSL Round 14",
     "date": "2026-05-31",
