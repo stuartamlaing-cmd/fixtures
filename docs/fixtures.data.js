b0689a6daadb5ab5298b0b958a1d03fb3df085a2
stuartamlaing-cmd <stuartamlaing@gmail.com>
2026-06-02 15:55:51 +0100

    Update fixtures.data.js

--- docs/fixtures.data.js
+++ docs/fixtures.data.js
@@ -1,10 +1,10 @@
-/* Generated 2026-06-02T14:55:19.218Z */
+/* Generated 2026-06-02T14:55:50.741Z */
 window.FIXTURES = [
   {
     "competition": "Major Semi Final",
     "date": "2026-06-06",
-    "timeUK": "",
-    "location": "",
+    "timeUK": "19:30",
+    "location": "National Cycling Centre",
     "team1": "",
     "team2": "",
     "watch": ""
