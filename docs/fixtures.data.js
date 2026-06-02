039b88f63ca339cf0c552f53d91e62fc39e4575c
stuartamlaing-cmd <stuartamlaing@gmail.com>
2026-06-02 16:01:00 +0100

    Update fixtures.data.js

--- docs/fixtures.data.js
+++ docs/fixtures.data.js
@@ -1,5 +1,14 @@
-/* Generated 2026-06-02T15:00:22.314Z */
+/* Generated 2026-06-02T15:01:00.181Z */
 window.FIXTURES = [
+  {
+    "competition": "Minor Semi Final",
+    "date": "2026-06-06",
+    "timeUK": "15:00",
+    "location": "Sir David Wallace Arena",
+    "team1": "",
+    "team2": "",
+    "watch": ""
+  },
   {
     "competition": "Major Semi Final",
     "date": "2026-06-06",
@@ -7,15 +16,6 @@ window.FIXTURES = [
     "location": "National Cycling Centre",
     "team1": "AO Manchester Thunder",
     "team2": "London Pulse",
-    "watch": ""
-  },
-  {
-    "competition": "Minor Semi Final",
-    "date": "2026-06-06",
-    "timeUK": "",
-    "location": "",
-    "team1": "",
-    "team2": "",
     "watch": ""
   }
 ];
