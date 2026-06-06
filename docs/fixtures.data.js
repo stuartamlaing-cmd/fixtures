2cd8d6f627c7cca5623ca31a3a19ec4910c05b50
stuartamlaing-cmd <stuartamlaing@gmail.com>
2026-06-06 18:19:56 +0100

    Update fixtures.data.js

--- docs/fixtures.data.js
+++ docs/fixtures.data.js
@@ -1,14 +1,5 @@
-/* Generated 2026-06-05T15:35:27.022Z */
+/* Generated 2026-06-06T17:19:55.935Z */
 window.FIXTURES = [
-  {
-    "competition": "Minor Semi Final",
-    "date": "2026-06-06",
-    "timeUK": "15:00",
-    "location": "Sir David Wallace Arena",
-    "team1": "Loughborough Lightning",
-    "team2": "Nottingham Forest",
-    "watch": "Sky Sports; Sky Sports YouTube; Netball Pass"
-  },
   {
     "competition": "Major Semi Final",
     "date": "2026-06-06",
