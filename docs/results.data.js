1800e9dc270a71d536e4abfb17348cd12dedb768
stuartamlaing-cmd <stuartamlaing@gmail.com>
2026-06-06 18:58:59 +0100

    Update results.data.js

--- docs/results.data.js
+++ docs/results.data.js
@@ -1,5 +1,16 @@
-/* Generated 2026-06-06T17:19:55.936Z */
+/* Generated 2026-06-06T17:58:57.285Z */
 window.RESULTS  = [
+  {
+    "competition": "Major Semi Final",
+    "date": "2026-06-06",
+    "timeUK": "17:30",
+    "location": "National Cycling Centre",
+    "team1": "AO Manchester Thunder",
+    "team2": "London Pulse",
+    "watch": "Sky Sports; Sky Sports YouTube; Netball Pass",
+    "score1": "60",
+    "score2": ""
+  },
   {
     "competition": "Minor Semi Final",
     "date": "2026-06-06",
@@ -9,7 +20,7 @@ window.RESULTS  = [
     "team2": "Nottingham Forest",
     "watch": "Sky Sports; Sky Sports YouTube; Netball Pass",
     "score1": "77",
-    "score2": ""
+    "score2": "57"
   },
   {
     "competition": "NSL Round 14",
