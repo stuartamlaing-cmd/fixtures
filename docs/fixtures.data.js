79e01d7bf63bb02d9f001c27ac786f8d0803cd53
stuartamlaing-cmd <stuartamlaing@gmail.com>
2026-06-06 18:58:57 +0100

    Update fixtures.data.js

--- docs/fixtures.data.js
+++ docs/fixtures.data.js
@@ -1,12 +1,2 @@
-/* Generated 2026-06-06T17:19:55.935Z */
-window.FIXTURES = [
-  {
-    "competition": "Major Semi Final",
-    "date": "2026-06-06",
-    "timeUK": "17:30",
-    "location": "National Cycling Centre",
-    "team1": "AO Manchester Thunder",
-    "team2": "London Pulse",
-    "watch": "Sky Sports; Sky Sports YouTube; Netball Pass"
-  }
-];
+/* Generated 2026-06-06T17:58:57.281Z */
+window.FIXTURES = [];
