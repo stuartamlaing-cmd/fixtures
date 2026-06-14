4a30169eeb4492d2747539ce8c4f780120c91f24
stuartamlaing-cmd <stuartamlaing@gmail.com>
2026-06-14 06:27:43 +0100

    Update fixtures.data.js

--- docs/fixtures.data.js
+++ docs/fixtures.data.js
@@ -1,9 +1,9 @@
-/* Generated 2026-06-14T05:25:26.460Z */
+/* Generated 2026-06-14T05:27:42.934Z */
 window.FIXTURES = [
   {
     "competition": "NSL 2026 Final",
     "date": "2026-06-20",
-    "timeUK": "TBC",
+    "timeUK": "15:00",
     "location": "TBC",
     "team1": "TBC",
     "team2": "TBC",
