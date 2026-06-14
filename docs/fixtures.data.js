40e2c68ea22f0eb1aede360d6d3d5a561ac3ef0f
stuartamlaing-cmd <stuartamlaing@gmail.com>
2026-06-14 06:25:26 +0100

    Update fixtures.data.js

--- docs/fixtures.data.js
+++ docs/fixtures.data.js
@@ -1,21 +1,12 @@
-/* Generated 2026-06-13T11:35:08.029Z */
+/* Generated 2026-06-14T05:25:26.460Z */
 window.FIXTURES = [
-  {
-    "competition": "NSL 2026 Preliminary finals",
-    "date": "2026-06-13",
-    "timeUK": "16:00",
-    "location": "Lee Valley Velo Park",
-    "team1": "London Pulse",
-    "team2": "Loughborough Lightning",
-    "watch": "Sky Sports; Sky Sports YouTube; Netball Pass"
-  },
   {
     "competition": "NSL 2026 Final",
     "date": "2026-06-20",
-    "timeUK": "",
+    "timeUK": "TBC",
     "location": "TBC",
     "team1": "TBC",
-    "team2": "",
-    "watch": ""
+    "team2": "TBC",
+    "watch": "TBC"
   }
 ];
