45474be4042710bbaaf63c59e454c6666ea312fc
stuartamlaing-cmd <stuartamlaing@gmail.com>
2026-06-14 06:31:13 +0100

    Update fixtures.data.js

--- docs/fixtures.data.js
+++ docs/fixtures.data.js
@@ -1,4 +1,4 @@
-/* Generated 2026-06-14T05:30:26.325Z */
+/* Generated 2026-06-14T05:31:12.979Z */
 window.FIXTURES = [
   {
     "competition": "NSL 2026 Final",
@@ -6,7 +6,7 @@ window.FIXTURES = [
     "timeUK": "15:00",
     "location": "Co-op Live",
     "team1": "AO Manchester Thunder",
-    "team2": "London",
-    "watch": "Sky Sports; Sky Sports YouTube; Netball Pass"
+    "team2": "London Pulse",
+    "watch": "Sky Sports; Sky Sports YouTube; Netball Pas"
   }
 ];
