a1dbd0f29c7f7e47879203b72f3602bdffd0bd7b
stuartamlaing-cmd <stuartamlaing@gmail.com>
2026-06-14 06:28:24 +0100

    Update fixtures.data.js

--- docs/fixtures.data.js
+++ docs/fixtures.data.js
@@ -1,11 +1,11 @@
-/* Generated 2026-06-14T05:27:42.934Z */
+/* Generated 2026-06-14T05:28:23.904Z */
 window.FIXTURES = [
   {
     "competition": "NSL 2026 Final",
     "date": "2026-06-20",
     "timeUK": "15:00",
-    "location": "TBC",
-    "team1": "TBC",
+    "location": "Co-op Live",
+    "team1": "AO Manchester Thunder",
     "team2": "TBC",
     "watch": "TBC"
   }
