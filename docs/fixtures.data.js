27ad36ad9bb0497fbfe9a05aeb39a0cbfa1bd159
stuartamlaing-cmd <stuartamlaing@gmail.com>
2026-07-23 16:07:58 +0100

    Update fixtures.data.js

--- docs/fixtures.data.js
+++ docs/fixtures.data.js
@@ -1,4 +1,4 @@
-/* Generated 2026-07-23T15:07:16.493Z */
+/* Generated 2026-07-23T15:07:57.563Z */
 window.FIXTURES = [
   {
     "competition": "Commonwealth Games 2026",
@@ -8,5 +8,14 @@ window.FIXTURES = [
     "team1": "New Zealand",
     "team2": "Scotland",
     "watch": "HBO Max"
+  },
+  {
+    "competition": "Commonwealth Games 2026",
+    "date": "2026-07-25",
+    "timeUK": "11:00",
+    "location": "",
+    "team1": "Australia",
+    "team2": "",
+    "watch": ""
   }
 ];
